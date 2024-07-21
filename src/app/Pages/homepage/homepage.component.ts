05e64e760a312ae6419f4a2523b4d9dd3decd955
Gipfelienjoyer <luis.haenny@proton.me>
2024-07-21 17:53:58 +0200

    os transfer

---
+++ src/app/Pages/homepage/homepage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+
+@Component({
+  selector: 'Homepage',
+  standalone: true,
+  imports: [],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.scss'
+})
+export class Homepage {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Homepage } from "./Pages/homepage/homepage.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: Homepage},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
