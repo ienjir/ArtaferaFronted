a6f079d2218e03c962a32d19de12788ba41e4bde
ienjir <info@ienjir.dev>
2024-11-23 15:25:21 +0100

    not truly but lets start again, so: Inital commit

--- src/app/Pages/homepage/homepage.component.ts
+++ src/app/Pages/homepage/homepage.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
+import {TranslocoPipe} from "@jsverse/transloco";
 
 @Component({
   selector: 'Homepage',
   standalone: true,
   imports: [
-    CarouselModule
+    CarouselModule,
+    TranslocoPipe
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
