6a9c00569ca255099f5f79b4dd29e6f94eae18d9
ienjir <info@ienjir.dev>
2025-03-12 07:21:05 +0100

    Added basic LoginPage component + routing

---
+++ src/app/Pages/loginpage/loginpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'LoginPage',
+  standalone: true,
+  imports: [],
+  templateUrl: './loginpage.component.html',
+  styleUrl: './loginpage.component.scss'
+})
+export class LoginPage {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Homepage } from "./Pages/homepage/homepage.component";
+import {LoginPage} from "./Pages/loginpage/loginpage.component";
 
 export const routes: Routes = [
   { path: '', component: Homepage},
-  { path: 'home', redirectTo: '', pathMatch: 'full'}
+  { path: 'home', redirectTo: '', pathMatch: 'full'},
+  { path: 'login', component: LoginPage}
 ];
