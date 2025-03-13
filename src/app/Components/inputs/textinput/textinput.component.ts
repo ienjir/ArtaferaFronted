713cebdea436e515f3ee5dcfe079771844a1c687
ienjir <info@ienjir.dev>
2025-03-13 16:01:32 +0100

    Added loginpage

---
+++ src/app/Components/inputs/textinput/textinput.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textinput',
+  standalone: true,
+  imports: [],
+  templateUrl: './textinput.component.html',
+  styleUrl: './textinput.component.scss'
+})
+export class TextinputComponent {
+
+}
