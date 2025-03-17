824eb387c7e59e435f29deede20ffbfbd6574546
ienjir <info@ienjir.dev>
2025-03-17 18:08:59 +0100

    Fixed floating label

--- src/app/Components/inputs/textinput/textinput.component.ts
+++ src/app/Components/inputs/textinput/textinput.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ContentChild, ElementRef, Input, Renderer2} from '@angular/core';
 
 @Component({
-  selector: 'app-textinput',
+  selector: 'Textinput',
   standalone: true,
   imports: [],
   templateUrl: './textinput.component.html',
   styleUrl: './textinput.component.scss'
 })
-export class TextinputComponent {
+export class FloatingLabelComponent implements AfterViewInit {
+  @Input() label!: string;
+  @ContentChild('input', { static: false }) inputElement!: ElementRef;
+  isFocused = false;
+  hasValue = false;
 
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    if (this.inputElement) {
+      this.renderer.listen(this.inputElement.nativeElement, 'focus', () => {
+        this.isFocused = true;
+      });
+      this.renderer.listen(this.inputElement.nativeElement, 'blur', () => {
+        this.isFocused = false;
+        this.hasValue = !!this.inputElement.nativeElement.value;
+      });
+      this.hasValue = !!this.inputElement.nativeElement.value;
+    }
+  }
 }
--- src/app/Pages/loginpage/loginpage.component.ts
+++ src/app/Pages/loginpage/loginpage.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NavigationBar} from "../../Components/Navigation/navigation-bar/navigation-bar.component";
+import {FloatingLabelComponent} from "../../Components/inputs/textinput/textinput.component";
 
 @Component({
   selector: 'LoginPage',
   standalone: true,
   imports: [
-    NavigationBar
+    NavigationBar,
+    FloatingLabelComponent
   ],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.scss'
