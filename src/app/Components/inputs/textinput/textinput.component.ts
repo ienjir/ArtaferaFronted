ee66066d14a27244fb298dc573d62e1e07f723b3
ienjir <info@ienjir.dev>
2025-03-19 08:59:45 +0100

    Cleanup done

--- src/app/Components/inputs/textinput/textinput.component.ts
+++ src/app/Components/inputs/textinput/textinput.component.ts
@@ -12,8 +12,6 @@ export class FloatingLabelComponent implements AfterViewInit {
   @ContentChild('input', { static: false }) inputElement!: ElementRef;
   isFocused = false;
   hasValue = false;
-  isFocusVisible = false;
-  isFocusClick = false;
 
   constructor(private renderer: Renderer2) {}
 
@@ -21,47 +19,26 @@ export class FloatingLabelComponent implements AfterViewInit {
     if (this.inputElement) {
       const nativeInput = this.inputElement.nativeElement;
 
-      // Listen for focus from any source
       this.renderer.listen(nativeInput, 'focus', () => {
-        console.log("Focused: " + this.isFocused)
         this.isFocused = true;
       });
 
-      // Listen for blur event
       this.renderer.listen(nativeInput, 'blur', () => {
-        console.log("Blur");
         this.isFocused = false;
         this.hasValue = !!nativeInput.value;
-        this.isFocusVisible = false;
-        this.isFocusClick = false;
-
-        // Reset border when losing focus
         nativeInput.style.border = "";
       });
 
-      // Detect focus from keyboard
       this.renderer.listen(nativeInput, 'keydown', (event: KeyboardEvent) => {
         if (event.key === 'Tab') {
-          this.isFocusVisible = true;
-          this.isFocusClick = false;
-          nativeInput.style.border = "3px solid green"
-          console.log("Keyboard")
-          console.log("IsFocusVisible: " + this.isFocusVisible)
-          console.log("IsFocusClick: " + this.isFocusClick)
+          nativeInput.style.border = "3px solid var(--Focus)"
         }
       });
 
-      // Detect focus from mouse
       this.renderer.listen(nativeInput, 'mousedown', () => {
-        this.isFocusVisible = false;
-        this.isFocusClick = true;
-        nativeInput.style.border = "3px solid red"
-        console.log("Mouse")
-        console.log("IsFocusVisible: " + this.isFocusVisible)
-        console.log("IsFocusClick: " + this.isFocusClick)
+        nativeInput.style.border = "3px solid var(--Main)"
       });
 
-      // Set initial state
       this.hasValue = !!nativeInput.value;
     }
   }
