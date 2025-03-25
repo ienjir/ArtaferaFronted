ec5cdc20408635726f4b2a066af83a58cc7afa85
ienjir <info@ienjir.dev>
2025-03-25 18:43:53 +0100

    Added basic login function

--- src/app/Pages/loginpage/loginpage.component.ts
+++ src/app/Pages/loginpage/loginpage.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
-import {NavigationBar} from "../../Components/Navigation/navigation-bar/navigation-bar.component";
-import {FloatingLabelComponent} from "../../Components/inputs/textinput/textinput.component";
-import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
-import {TranslocoPipe} from "@jsverse/transloco";
+import { Component, inject } from '@angular/core';
+import { NavigationBar } from "../../Components/Navigation/navigation-bar/navigation-bar.component";
+import { FloatingLabelComponent } from "../../Components/inputs/textinput/textinput.component";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { TranslocoPipe } from "@jsverse/transloco";
+import { AuthService } from "../../Services/login/auth-service.service";
+import { Observable } from "rxjs";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'LoginPage',
@@ -11,15 +14,28 @@ import {TranslocoPipe} from "@jsverse/transloco";
     NavigationBar,
     FloatingLabelComponent,
     ReactiveFormsModule,
-    TranslocoPipe
+    TranslocoPipe,
+    HttpClientModule // Ensure HttpClientModule is imported
   ],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.scss'
 })
 export class LoginPage {
-  loginForm = new FormGroup({
+  private authService = inject(AuthService);
 
+  loginForm = new FormGroup({
     email: new FormControl(''),
     password: new FormControl(''),
-  })
+  });
+
+  submitLogin() {
+    const { email, password } = this.loginForm.value;
+
+    if (email && password) {
+      this.authService.postLogin(email, password).subscribe({
+        next: response => console.log("Login successful:", response),
+        error: err => console.error("Login failed:", err)
+      });
+    }
+  }
 }
---
+++ src/app/Services/login/auth-service.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  postLogin(email: string, password: string): Observable<any> {
+    const payload = {
+      "email": email,
+      "password": password
+    }
+
+    return  this.http.post(`${this.apiUrl}/auth/login`, payload);
+  }
+}
