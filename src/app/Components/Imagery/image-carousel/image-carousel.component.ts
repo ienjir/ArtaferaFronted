6e9e3654bcc44283b197399b1381885df6f0ce23
ienjir <info@ienjir.dev>
2025-05-05 16:37:37 +0200

    Fixed image carousel

--- src/app/Components/Imagery/image-carousel/image-carousel.component.ts
+++ src/app/Components/Imagery/image-carousel/image-carousel.component.ts
@@ -1,25 +1,126 @@
-import {Component, Input} from '@angular/core';
-import {NgForOf} from "@angular/common";
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { NgForOf, NgClass } from "@angular/common";
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'ImageCarousel',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgClass
   ],
   templateUrl: './image-carousel.component.html',
-  styleUrl: './image-carousel.component.scss'
+  styleUrl: './image-carousel.component.scss',
+  animations: [
+    trigger('slideAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('600ms ease-in', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('600ms ease-out', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
-export class ImageCarousel {
+export class ImageCarousel implements OnInit, OnDestroy {
   currentSlideIndex = 0;
+  previousSlideIndex = 0;
+  slideDirection = 'next';
+  isAnimating = false;
+  autoPlayInterval: any;
 
-  @Input() slides: {image: string, caption: string}[] = [];
+  @Input() slides: { image: string, caption: string }[] = [
+    {image: "https://picsum.photos/id/236/1000/1400", caption: "Beautiful mountain landscape"},
+    {image: "https://picsum.photos/id/238/1000/1400", caption: "Serene lake view"},
+    {image: "https://picsum.photos/id/239/1000/1400", caption: "Majestic forest scenery"},
+  ];
+
+  @Input() autoPlay = true;
+  @Input() interval = 5000;
+
+  ngOnInit() {
+    if (this.autoPlay) {
+      this.startAutoPlay();
+    }
+  }
+
+  startAutoPlay() {
+    this.autoPlayInterval = setInterval(() => {
+      this.nextSlide();
+    }, this.interval);
+  }
+
+  stopAutoPlay() {
+    if (this.autoPlayInterval) {
+      clearInterval(this.autoPlayInterval);
+    }
+  }
 
   nextSlide() {
+    if (this.isAnimating) return;
+
+    this.isAnimating = true;
+    this.slideDirection = 'next';
+    this.previousSlideIndex = this.currentSlideIndex;
     this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slides.length;
+
+    setTimeout(() => {
+      this.isAnimating = false;
+    }, 600); // Match this with your CSS transition duration
   }
 
   prevSlide() {
+    if (this.isAnimating) return;
+
+    this.isAnimating = true;
+    this.slideDirection = 'prev';
+    this.previousSlideIndex = this.currentSlideIndex;
     this.currentSlideIndex = (this.currentSlideIndex - 1 + this.slides.length) % this.slides.length;
+
+    setTimeout(() => {
+      this.isAnimating = false;
+    }, 600); // Match this with your CSS transition duration
+  }
+
+  goToSlide(index: number) {
+    if (this.isAnimating || index === this.currentSlideIndex) return;
+
+    this.isAnimating = true;
+    this.slideDirection = index > this.currentSlideIndex ? 'next' : 'prev';
+    this.previousSlideIndex = this.currentSlideIndex;
+    this.currentSlideIndex = index;
+
+    setTimeout(() => {
+      this.isAnimating = false;
+    }, 600);
+  }
+
+  getSlideClass(index: number) {
+    if (index === this.currentSlideIndex) {
+      return 'active';
+    } else if (this.isAnimating && index === this.previousSlideIndex) {
+      return this.slideDirection === 'next' ? 'prev-slide' : 'next-slide';
+    } else if (this.isAnimating && index === this.currentSlideIndex) {
+      return 'new-active-slide';
+    }
+    return '';
+  }
+
+  // Track mouse events to pause auto-play when user interacts with carousel
+  onMouseEnter() {
+    if (this.autoPlay) {
+      this.stopAutoPlay();
+    }
+  }
+
+  onMouseLeave() {
+    if (this.autoPlay) {
+      this.startAutoPlay();
+    }
+  }
+
+  ngOnDestroy() {
+    this.stopAutoPlay();
   }
 }
--- src/app/Pages/homepage/homepage.component.ts
+++ src/app/Pages/homepage/homepage.component.ts
@@ -3,7 +3,6 @@ import {CarouselModule} from 'primeng/carousel';
 import {TranslocoPipe} from "@jsverse/transloco";
 import {NavigationBar} from "../../Components/Navigation/navigation-bar/navigation-bar.component";
 import {ImageCarousel} from "../../Components/Imagery/image-carousel/image-carousel.component";
-import {AuthService} from "../../Services/auth/auth.service";
 
 @Component({
   selector: 'Homepage',
@@ -17,17 +16,5 @@ import {AuthService} from "../../Services/auth/auth.service";
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
-export class Homepage implements OnInit {
-  private authService = inject(AuthService);
-
-  ngOnInit() {
-    this.authService.getUsers().subscribe({
-      next: () => {
-        console.log(this.authService)
-      },
-      error: err => {
-        console.error(err)
-      }
-    })
-  }
+export class Homepage {
 }
