502a99e6deadcfeac4593e03d03ac3022b7add17
ienjir <info@ienjir.dev>
2025-05-09 07:50:28 +0200

    Added label component

--- src/app/Components/Display/art-preview/art-preview.component.ts
+++ src/app/Components/Display/art-preview/art-preview.component.ts
@@ -1,9 +1,12 @@
 import {Component, Input} from '@angular/core';
+import {Label} from "../label/label.component";
 
 @Component({
   selector: 'ArtPreview',
   standalone: true,
-  imports: [],
+  imports: [
+    Label
+  ],
   templateUrl: './art-preview.component.html',
   styleUrl: './art-preview.component.scss'
 })
