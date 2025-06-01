876355c28e4ed0b6498e739e9eee4ab8343e7d0e
ienjir <info@ienjir.dev>
2025-06-01 18:38:50 +0200

    Added ssr back (hopefully)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import {ApplicationConfig, isDevMode} from '@angular/core';
 import {provideRouter} from '@angular/router';
+
 import {routes} from './app.routes';
+import {provideClientHydration} from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import {TranslocoHttpLoader} from './transloco-loader';
 import {provideTransloco} from '@jsverse/transloco';
@@ -9,6 +11,7 @@ import {AuthInterceptor} from "./Interceptors/auth.interceptor";
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideClientHydration(),
     provideHttpClient(),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     provideTransloco({
