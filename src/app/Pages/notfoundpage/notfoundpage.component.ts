d2316087a8180a54aa72295ce9747cb7e1b2525f
ienjir <info@ienjir.dev>
2025-06-17 17:27:33 +0200

    Added simple 404 page

--- src/app/Pages/notfoundpage/notfoundpage.component.ts
+++ src/app/Pages/notfoundpage/notfoundpage.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {NavigationBar} from "../../Components/Navigation/navigation-bar/navigation-bar.component";
+import {TranslocoPipe, TranslocoService} from "@jsverse/transloco";
 
 @Component({
   selector: 'NotFoundPage',
   standalone: true,
   imports: [
-    NavigationBar
+    NavigationBar,
+    TranslocoPipe
   ],
   templateUrl: './notfoundpage.component.html',
   styleUrl: './notfoundpage.component.scss'
 })
 export class NotFoundPageComponent {
-
 }
