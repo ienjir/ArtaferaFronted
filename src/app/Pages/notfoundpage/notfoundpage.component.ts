ec4a191ed208de6e13214ff75553d15e9a5f3fb5
ienjir <info@ienjir.dev>
2025-06-19 05:47:37 +0200

    Hopefully fixed favicon situation

--- src/app/Pages/notfoundpage/notfoundpage.component.ts
+++ src/app/Pages/notfoundpage/notfoundpage.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {NavigationBar} from "../../Components/Navigation/navigation-bar/navigation-bar.component";
 import {TranslocoPipe, TranslocoService} from "@jsverse/transloco";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'NotFoundPage',
@@ -13,4 +14,8 @@ import {TranslocoPipe, TranslocoService} from "@jsverse/transloco";
   styleUrl: './notfoundpage.component.scss'
 })
 export class NotFoundPageComponent {
-}
+  constructor(private router: Router) {}
+
+  redirectTo() {
+    this.router.navigate(['/']);
+  }}
