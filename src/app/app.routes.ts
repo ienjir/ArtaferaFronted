e75d0d49e14ccc69dbe84c9e7aa60700341ca006
ienjir <info@ienjir.dev>
2025-06-20 22:00:40 +0200

    Added basic default layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,29 @@
-import { Routes } from '@angular/router';
-import { Homepage } from "./Pages/homepage/homepage.component";
+import {Routes} from '@angular/router';
+import {Homepage} from "./Pages/homepage/homepage.component";
 import {LoginPage} from "./Pages/loginpage/loginpage.component";
 import {AuthGuard} from "./Services/auth/auth-guard.service";
 import {NotFoundPageComponent} from "./Pages/notfoundpage/notfoundpage.component";
 import {ContactPageComponent} from "./Pages/contactpage/contactpage.component";
+import {DefaultLayoutComponent} from "./Pages/defaultLayout/defaultlayout.component";
 
 export const routes: Routes = [
-  { path: '', component: Homepage},
-  { path: 'home', redirectTo: '', pathMatch: 'full'},
-  { path: 'login', component: LoginPage},
-  { path: 'kontakt', component: ContactPageComponent },
-  { path: '**', component: NotFoundPageComponent}
-];
+    {
+      path: '',
+      component: DefaultLayoutComponent,
+      children: [
+        {path: 'login', component: LoginPage},
+        {path: 'kontakt', component: ContactPageComponent},
+        {path: '**', component: NotFoundPageComponent},
+      ]
+    },
+    {
+      path: '', component:
+      Homepage
+    },
+    {
+      path: 'home', redirectTo:
+        '', pathMatch:
+        'full'
+    },
+  ]
+;
