9905ec2621681b2a3f821ec3a5250b88ff7c9bf9
ienjir <info@ienjir.dev>
2025-06-20 07:47:50 +0200

    Added basic contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Homepage } from "./Pages/homepage/homepage.component";
 import {LoginPage} from "./Pages/loginpage/loginpage.component";
 import {AuthGuard} from "./Services/auth/auth-guard.service";
 import {NotFoundPageComponent} from "./Pages/notfoundpage/notfoundpage.component";
+import {ContactPageComponent} from "./Pages/contactpage/contactpage.component";
 
 export const routes: Routes = [
   { path: '', component: Homepage},
   { path: 'home', redirectTo: '', pathMatch: 'full'},
   { path: 'login', component: LoginPage},
+  { path: 'kontakt', component: ContactPageComponent },
   { path: '**', component: NotFoundPageComponent}
 ];
